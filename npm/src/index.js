87eee4dffae566e94937d26d01e09652c493732b
vitaliytv <v@nitra.ai>
2025-03-31 22:28:51 +0300

    export logging in Fastify  2.2.2

--- npm/src/index.js
+++ npm/src/index.js
@@ -5,16 +5,18 @@ import { env, exit } from 'node:process'
 
 const port = Number(env.PORT) || 8080
 
+export const log = getLogger()
+
 export const app = fastify({
   loggerInstance: getLogger(),
-  bodyLimit: (env.BODY_LIMIT_MB || 1) * 1024 * 1024,
+  bodyLimit: Number(env.BODY_LIMIT_MB || 1) * 1024 * 1024,
   http2: !!env.K_SERVICE // Запускаємо з http2 якщо в Cloud Run
 })
 
 app.register(fastifySensible) // для reply.badRequest(`Not found url: ${req.url} ...`)
 
 // Опрацювання запитів OPTIONS
-app.options('/*', async (req, reply) => {
+app.options('/*', (req, reply) => {
   // + setHeaders в preHandler
   reply.header('Access-Control-Max-Age', 86400)
   reply.header('Cache-Control', 'public, max-age=86400')
@@ -44,17 +46,17 @@ app.addHook('preHandler', (req, reply, done) => {
 export function listen() {
   app
     .listen({ port, host: '0.0.0.0' })
-    .then(address => console.log(`🚀 Server ready at ${address}`))
+    .then(address => log.info(`🚀 Server ready at ${address}`))
     .catch(error => {
-      console.error('Error starting server:', error)
+      log.error('Error starting server:', error)
       exit(1)
     })
 }
 
 /**
  *
- * @param req
- * @param reply
+ * @param {*} req req
+ * @param {*} reply reply
  */
 function setHeaders(req, reply) {
   let host = env.ORIGIN || req.headers.origin || req.headers.referer
